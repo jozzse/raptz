6e87b9d5f41996c484ca25e91f8e8de89a1bcb6e
Jonas Zetterberg <jonas.zetterberg@tobii.com>
2014-10-30 11:15:37 +0100

    Unmount on exit

--- raptz.py
+++ raptz.py
@@ -7,22 +7,28 @@ import sys
 sys.path.append("/usr/share/pyshared/")
 
 from Raptz import Raptz, RaptzError
+import atexit
+
+
+def raptz_exit(raptz):
+	raptz.umount_all()
 
 if __name__=="__main__":
 	debug = False
 	ret = 1
 	try:
 		raptz = Raptz()
+		atexit.register(raptz_exit, raptz)
 		debug = raptz.args.debug
 		ret = raptz.start()
-	except RaptzError, why:
-		print ""
-		print ""
-		print "Failed to create sysroot: " + str(why)
 	except KeyboardInterrupt:
 		print ""
 		print ""
 		print "It looks like you have canceled the Sysroot installation. Installation is not complete."
+	except RaptzError, why:
+		print ""
+		print ""
+		print "Failed to create sysroot: " + str(why)
 	except BaseException,why:
 		print ""
 		print ""
