b39ebd2e96a96818d7a2c43276bb98a118fd0928
Jonas Zetterberg <jozz@jozz.se>
2016-02-22 21:39:03 +0100

    chroot: Fix resolv.conf handling

--- libraptz/chroot.py
+++ libraptz/chroot.py
@@ -11,6 +11,8 @@ import shutil
 
 QEMU_BIN="qemu-arm-static"
 
+RESOLV_CONF_PATH="/etc/resolv.conf"
+
 class ChRoot:
 	RCDFILE_CONTENT="""#!/bin/dash\nexit 101\n"""
 	def __init__(self, host):
@@ -20,14 +22,13 @@ class ChRoot:
 		progs.register("chroot")
 		self._qemudst = config.rootfs(progs.get(QEMU_BIN))
 
-	def run(self, cmds, env=None, stdoutfunc=None, stderrfunc=None, *kargs):
+	def run(self, cmds, env={}, stdoutfunc=None, stderrfunc=None, *kargs):
 		stdout = self._host.stdoutfd()
 		stderr = self._host.stderrfd()
 		renv = os.environ
 		cmds[0] = progs.get(cmds[0])
-		if env:
-			for key, value in env.items():
-				renv[key] = value
+		for key, value in env.items():
+			renv[key] = value
 		for key, value in self._host.fs.env().items():
 			renv[key] = value
 
@@ -49,8 +50,16 @@ class ChRoot:
 		shutil.copy(progs.get(QEMU_BIN), self._qemudst)
 		open(self._rcddst, "w").write(self.RCDFILE_CONTENT)
 		os.chmod(self._rcddst, 0755)
+		tconf=config.rootfs(RESOLV_CONF_PATH)
+		if os.path.exists(tconf):
+			os.rename(tconf, tconf+".real")
+			shutil.copy(RESOLV_CONF_PATH, tconf)
 
-	def _ch_setdown(self):
+	def _ch_teardown(self):
+		tconf=config.rootfs(RESOLV_CONF_PATH)
+		if os.path.exists(tconf+".real"):
+			os.unlink(tconf)
+			os.rename(tconf+".real", tconf)
 		if os.path.isfile(self._rcddst):
 			os.unlink(self._rcddst)
 		if os.path.isfile(self._qemudst):
@@ -66,32 +75,36 @@ class ChRoot:
 		for e in env:
 			oenv[e] = env[e]
 		ret = self.run(cmds, oenv, stdoutfunc, stderrfunc, *kargs)
-		self._ch_setdown()
+		self._ch_teardown()
 		return ret
 
 class FakeRoot(ChRoot):
 	RCDFILE_CONTENT="""#!/bin/dash\nexit 101\n"""
 	_qemusrc = "/usr/bin/qemu-arm-static"
 	def __init__(self, host):
+		progs.register("fakechroot")
+		progs.register("fakeroot")
 		ChRoot.__init__(self, host)
 
 	def run(self, cmds, env={}, stdoutfunc=None, stderrfunc=None, *kargs):
 		envfile = config.rootfs("/lib/fake.env")
-		fakecmd = [ "fakechroot", 
-			"fakeroot", 
+		fakecmd = [ "fakechroot",
+			"fakeroot",
 			"-s", envfile,
 			"-i", envfile,
 		]
 			#"-c", "fcr",
 		cmds = fakecmd + cmds
-		env["FAKECHROOT_EXCLUDE_PATH"] = self._host.fs.binds()
+#		env["FAKECHROOT_EXCLUDE_PATH"] =	":".join(self._host.fs.binds())
 		return ChRoot.run(self, cmds, env, stdoutfunc, stderrfunc, *kargs)
 
 	def chroot(self, cmds, env={}, stdoutfunc=None, stderrfunc=None, *kargs):
+		print(env)
 		self.copy_ld()
 		return ChRoot.chroot(self, cmds, env, stdoutfunc, stderrfunc, *kargs)
 
 	def copy_ld(self):
+		return
 		libdir=config.rootfs("lib")
 		ld = os.path.join(libdir, "ld-linux.so.3")
 		lddst = os.path.join(libdir, os.readlink(ld))
