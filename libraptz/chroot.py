2acc17daeeec57451491e252874fe21a73fa5d39
Jonas Zetterberg <jonas.zetterberg@flir.se>
2016-05-18 14:43:26 +0200

    Fix resolv.conf handling for resolvconf package

--- libraptz/chroot.py
+++ libraptz/chroot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import hashlib
 import sys
 import os
 import select
@@ -12,6 +13,15 @@ import shutil
 QEMU_BIN="qemu-arm-static"
 
 RESOLV_CONF_PATH="/etc/resolv.conf"
+
+def hashfile(filename):
+	if not os.path.exists(filename):
+		return None
+	ret = hashlib.md5()
+	with open(filename, "r") as f:
+		for line in f:
+			ret.update(line)
+	return ret.hexdigest()
 
 class ChRoot:
 	RCDFILE_CONTENT="""#!/bin/dash\nexit 101\n"""
@@ -29,23 +39,41 @@ class ChRoot:
 			renv[key] = value
 		for key, value in self._host.fs.env().items():
 			renv[key] = value
-
+		
 		return call(cmds, env=renv, stdout=config.stdout, stderr=config.stderr)
 
 	def _ch_setup(self):
 		shutil.copy(progs.get(QEMU_BIN), self._qemudst)
 		open(self._rcddst, "w").write(self.RCDFILE_CONTENT)
 		os.chmod(self._rcddst, 0755)
+		# resolv.conf handling
+		# If there is a resolv.conf move it to resolv.conf.real
+		# Move host resolv.conf to target resolv.conf and store its md5 in self._resolvhash
 		tconf=config.rootfs(RESOLV_CONF_PATH)
 		if os.path.exists(tconf):
 			os.rename(tconf, tconf+".real")
-			shutil.copy(RESOLV_CONF_PATH, tconf)
+		shutil.copy(RESOLV_CONF_PATH, tconf)
+		open(tconf, "a").write("""
+# This is the Hosts resolv.conf.
+# If changed in any way the target resolv.conf will not be restored.
+""")
+		self._resolvhash = hashfile(tconf)
 
 	def _ch_teardown(self):
-		tconf=config.rootfs(RESOLV_CONF_PATH)
-		if os.path.exists(tconf+".real"):
+		# resolv.conf handling
+		# Check that resolv.conf is a file and hash matches stored hash and is a file
+		# if there is change. Leave as is and remove resolv.conf.real
+		# if there is not change. Restore .real file if it exists
+		tconf = config.rootfs(RESOLV_CONF_PATH)
+		if not os.path.islink(tconf) and hashfile(tconf) == self._resolvhash:
 			os.unlink(tconf)
-			os.rename(tconf+".real", tconf)
+			if os.path.exists(tconf+".real"):
+				os.rename(tconf+".real", tconf)
+		else:
+			if os.path.exists(tconf+".real"):
+				os.unlink(tconf + ".real")
+		del self._resolvhash
+
 		if os.path.isfile(self._rcddst):
 			os.unlink(self._rcddst)
 		if os.path.isfile(self._qemudst):
